00d72b2bd624b63b10626755111eab8e59ee7f37
thong8888 <97076698+thong8888@users.noreply.github.com>
2023-07-12 09:24:39 +0700

    shopping

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "shoppingonline.xurgzpg.mongodb.net",
+  DB_USER: "huythong88888",
+  DB_PASS: "20112002",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "huythong8888@gmail.com",
+  EMAIL_PASS: "mlzbfivkxmtnnsgb",
+  JWT_SECRET: "<jwt_secret>",
+  JWT_EXPIRES: "86400000", // in milliseconds
+};
+module.exports = MyConstants;
